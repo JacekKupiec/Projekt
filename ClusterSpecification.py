75b8964d46043b52d98bdadaf2f844499cc19990
JacekKupiec <jacek.kupiec@o2.pl>
2018-05-28 12:13:29 +0200

    Poprawa literówki w specyfikacji klastra

--- ClusterSpecification.py
+++ ClusterSpecification.py
@@ -5,4 +5,4 @@ import tensorflow as tf
 
 PS_HOSTS = ['192.168.1.12:2222']
 WORKER_HOSTS = ['192.168.1.20:2223']
-CLUSTER_SPEC = tf.train.ClustSpec({'ps': PS_HOSTS, 'worker': WORKER_HOSTS})
+CLUSTER_SPEC = tf.train.ClusterSpec({'ps': PS_HOSTS, 'worker': WORKER_HOSTS})
