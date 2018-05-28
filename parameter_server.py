0b8114bed9af96395414b1a67b7d4a52ed47a7bc
Jacek Kupiec <jacek.kupiec@o2.pl>
2018-05-28 15:03:47 +0200

    Serwer parametrów nie używa GPU

--- parameter_server.py
+++ parameter_server.py
@@ -3,6 +3,8 @@
 
 import tensorflow as tf
 import ClusterSpecification
+import os
 
+os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
 server = tf.train.Server(ClusterSpecification.CLUSTER_SPEC, job_name='ps', task_index=0)
 server.join()
