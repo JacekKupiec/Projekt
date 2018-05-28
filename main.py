c27b105169d0d1f29f7c8f02af73998202e75b66
Jacek Kupiec <jacek.kupiec@o2.pl>
2018-05-28 14:56:13 +0200

    Wersja z działajacymi robotnikami i serwerem parametrow

--- main.py
+++ main.py
@@ -153,44 +153,46 @@ def main(_):
 
   train_size = train_labels.shape[0]
 
-#Konfiguracja serwera
-  server = tf.train.Server(ClusterSpecification.CLUSTER_SPEC, job_name='worker', task_index=0)
+  #Konfiguracja serwera
+  server = tf.train.Server(ClusterSpecification.CLUSTER_SPEC, job_name='worker', task_index=FLAGS.index)
 
   # This is where training samples and labels are fed to the graph.
   # These placeholder nodes will be fed a batch of training data at each
   # training step using the {feed_dict} argument to the Run() call below.
+  train_data_node = tf.placeholder(
+      data_type(),
+      shape=(BATCH_SIZE, IMAGE_SIZE, IMAGE_SIZE, NUM_CHANNELS))
+  train_labels_node = tf.placeholder(tf.int64, shape=(BATCH_SIZE,))
+  eval_data = tf.placeholder(data_type(), shape=(EVAL_BATCH_SIZE, IMAGE_SIZE, IMAGE_SIZE, NUM_CHANNELS))
+
   with tf.device(tf.train.replica_device_setter(cluster=ClusterSpecification.CLUSTER_SPEC)):
-      train_data_node = tf.placeholder(
-          data_type(),
-          shape=(BATCH_SIZE, IMAGE_SIZE, IMAGE_SIZE, NUM_CHANNELS))
-      train_labels_node = tf.placeholder(tf.int64, shape=(BATCH_SIZE,))
-      eval_data = tf.placeholder(
-          data_type(),
-          shape=(EVAL_BATCH_SIZE, IMAGE_SIZE, IMAGE_SIZE, NUM_CHANNELS))
+    # The variables below hold all the trainable weights. They are passed an
+    # initial value which will be assigned when we call:
+    # {tf.global_variables_initializer().run()}
+    conv1_weights = tf.Variable(
+        tf.truncated_normal([5, 5, NUM_CHANNELS, 32],  # 5x5 filter, depth 32.
+                            stddev=0.1,
+                            seed=SEED, dtype=data_type()))
+    conv1_biases = tf.Variable(tf.zeros([32], dtype=data_type()))
 
-      # The variables below hold all the trainable weights. They are passed an
-      # initial value which will be assigned when we call:
-      # {tf.global_variables_initializer().run()}
-      conv1_weights = tf.Variable(
-          tf.truncated_normal([5, 5, NUM_CHANNELS, 32],  # 5x5 filter, depth 32.
-                              stddev=0.1,
-                              seed=SEED, dtype=data_type()))
-      conv1_biases = tf.Variable(tf.zeros([32], dtype=data_type()))
+    conv2_weights = tf.Variable(tf.truncated_normal(
+        [5, 5, 32, 64], stddev=0.1,
+        seed=SEED, dtype=data_type()))
+    conv2_biases = tf.Variable(tf.constant(0.1, shape=[64], dtype=data_type()))
 
-      conv2_weights = tf.Variable(tf.truncated_normal(
-          [5, 5, 32, 64], stddev=0.1,
-          seed=SEED, dtype=data_type()))
-      conv2_biases = tf.Variable(tf.constant(0.1, shape=[64], dtype=data_type()))
+    fc1_weights = tf.Variable(  # fully connected, depth 512.
+        tf.truncated_normal([IMAGE_SIZE // 4 * IMAGE_SIZE // 4 * 64, 512],
+                            stddev=0.1,
+                            seed=SEED,
+                            dtype=data_type()))
+    fc1_biases = tf.Variable(tf.constant(0.1, shape=[512], dtype=data_type()))
 
-      fc1_weights = tf.Variable(  # fully connected, depth 512.
-          tf.truncated_normal([IMAGE_SIZE // 4 * IMAGE_SIZE // 4 * 64, 512],
-                              stddev=0.1,
-                              seed=SEED,
-                              dtype=data_type()))
-      fc1_biases = tf.Variable(tf.constant(0.1, shape=[512], dtype=data_type()))
+    fc2_weights = tf.Variable(tf.truncated_normal([512, NUM_LABELS], stddev=0.1, seed=SEED, dtype=data_type()))
+    fc2_biases = tf.Variable(tf.constant(0.1, shape=[NUM_LABELS], dtype=data_type()))
 
-      fc2_weights = tf.Variable(tf.truncated_normal([512, NUM_LABELS], stddev=0.1, seed=SEED, dtype=data_type()))
-      fc2_biases = tf.Variable(tf.constant(0.1, shape=[NUM_LABELS], dtype=data_type()))
+    # Optimizer: set up a variable that's incremented once per batch and
+    # controls the learning rate decay.
+    batch = tf.Variable(0, dtype=data_type())
 
   # We will replicate the model structure for the training subgraph, as well
   # as the evaluation subgraphs, while sharing the trainable parameters.
@@ -199,69 +201,78 @@ def main(_):
     # 2D convolution, with 'SAME' padding (i.e. the output feature map has
     # the same size as the input). Note that {strides} is a 4D array whose
     # shape matches the data layout: [image index, y, x, depth].
-
-    conv = tf.nn.conv2d(data,
-                        conv1_weights,
-                        strides=[1, 1, 1, 1],
-                        padding='SAME')
-    # Bias and rectified linear non-linearity.
-    relu = tf.nn.relu(tf.nn.bias_add(conv, conv1_biases))
-    # Max pooling. The kernel size spec {ksize} also follows the layout of
-    # the data. Here we have a pooling window of 2, and a stride of 2.
-    pool = tf.nn.max_pool(relu,
-                          ksize=[1, 2, 2, 1],
-                          strides=[1, 2, 2, 1],
+    with tf.device(tf.train.replica_device_setter(
+        worker_device="/job:worker/task:%d" % FLAGS.index, 
+        cluster=ClusterSpecification.CLUSTER_SPEC)):
+        
+      conv = tf.nn.conv2d(data,
+                          conv1_weights,
+                          strides=[1, 1, 1, 1],
                           padding='SAME')
-    conv = tf.nn.conv2d(pool,
-                        conv2_weights,
-                        strides=[1, 1, 1, 1],
-                        padding='SAME')
-    relu = tf.nn.relu(tf.nn.bias_add(conv, conv2_biases))
-    pool = tf.nn.max_pool(relu,
-                          ksize=[1, 2, 2, 1],
-                          strides=[1, 2, 2, 1],
+      # Bias and rectified linear non-linearity.
+      relu = tf.nn.relu(tf.nn.bias_add(conv, conv1_biases))
+      # Max pooling. The kernel size spec {ksize} also follows the layout of
+      # the data. Here we have a pooling window of 2, and a stride of 2.
+      pool = tf.nn.max_pool(relu,
+                            ksize=[1, 2, 2, 1],
+                            strides=[1, 2, 2, 1],
+                            padding='SAME')
+      conv = tf.nn.conv2d(pool,
+                          conv2_weights,
+                          strides=[1, 1, 1, 1],
                           padding='SAME')
-    # Reshape the feature map cuboid into a 2D matrix to feed it to the
-    # fully connected layers.
-    pool_shape = pool.get_shape().as_list()
-    reshape = tf.reshape(pool, [pool_shape[0], pool_shape[1] * pool_shape[2] * pool_shape[3]])
-    # Fully connected layer. Note that the '+' operation automatically
-    # broadcasts the biases.
-    hidden = tf.nn.relu(tf.matmul(reshape, fc1_weights) + fc1_biases)
-    # Add a 50% dropout during training only. Dropout also scales
-    # activations such that no rescaling is needed at evaluation time.
-    if train:
-      hidden = tf.nn.dropout(hidden, 0.5, seed=SEED)
-    return tf.matmul(hidden, fc2_weights) + fc2_biases
+      relu = tf.nn.relu(tf.nn.bias_add(conv, conv2_biases))
+      pool = tf.nn.max_pool(relu,
+                            ksize=[1, 2, 2, 1],
+                            strides=[1, 2, 2, 1],
+                            padding='SAME')
+      # Reshape the feature map cuboid into a 2D matrix to feed it to the
+      # fully connected layers.
+      pool_shape = pool.get_shape().as_list()
+      reshape = tf.reshape(pool, [pool_shape[0], pool_shape[1] * pool_shape[2] * pool_shape[3]])
+      # Fully connected layer. Note that the '+' operation automatically
+      # broadcasts the biases.
+      hidden = tf.nn.relu(tf.matmul(reshape, fc1_weights) + fc1_biases)
+      # Add a 50% dropout during training only. Dropout also scales
+      # activations such that no rescaling is needed at evaluation time.
+      if train:
+        hidden = tf.nn.dropout(hidden, 0.5, seed=SEED)
 
-  # Training computation: logits + cross-entropy loss.
-  logits = model(train_data_node, True)
-  loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(labels=train_labels_node, logits=logits))
+      mmm = tf.matmul(hidden, fc2_weights) + fc2_biases
 
-  # L2 regularization for the fully connected parameters.
-  regularizers = (tf.nn.l2_loss(fc1_weights) + tf.nn.l2_loss(fc1_biases) +
-                  tf.nn.l2_loss(fc2_weights) + tf.nn.l2_loss(fc2_biases))
-  # Add the regularization term to the loss.
-  loss += 5e-4 * regularizers
+    return mmm
 
-  # Optimizer: set up a variable that's incremented once per batch and
-  # controls the learning rate decay.
-  batch = tf.Variable(0, dtype=data_type())
-  # Decay once per epoch, using an exponential schedule starting at 0.01.
-  learning_rate = tf.train.exponential_decay(
-      0.01,                # Base learning rate.
-      batch * BATCH_SIZE,  # Current index into the dataset.
-      train_size,          # Decay step.
-      0.95,                # Decay rate.
-      staircase=True)
-  # Use simple momentum for the optimization.
-  optimizer = tf.train.MomentumOptimizer(learning_rate, 0.9).minimize(loss, global_step=batch)
 
-  # Predictions for the current training minibatch.
-  train_prediction = tf.nn.softmax(logits)
+  with tf.device(tf.train.replica_device_setter(
+      worker_device="/job:worker/task:%d" % FLAGS.index, 
+      cluster=ClusterSpecification.CLUSTER_SPEC)):
 
-  # Predictions for the test and validation, which we'll compute less often.
-  eval_prediction = tf.nn.softmax(model(eval_data))
+    # Training computation: logits + cross-entropy loss.
+    logits = model(train_data_node, True)
+    loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(labels=train_labels_node, logits=logits))
+
+    # L2 regularization for the fully connected parameters.
+    regularizers = (tf.nn.l2_loss(fc1_weights) + tf.nn.l2_loss(fc1_biases) +
+                    tf.nn.l2_loss(fc2_weights) + tf.nn.l2_loss(fc2_biases))
+    # Add the regularization term to the loss.
+    loss += 5e-4 * regularizers
+
+    # Decay once per epoch, using an exponential schedule starting at 0.01.
+    learning_rate = tf.train.exponential_decay(
+        0.01,                # Base learning rate.
+        batch * BATCH_SIZE,  # Current index into the dataset.
+        train_size,          # Decay step.
+        0.95,                # Decay rate.
+        staircase=True)
+
+    # Use simple momentum for the optimization.
+    optimizer = tf.train.MomentumOptimizer(learning_rate, 0.9).minimize(loss, global_step=batch)
+
+    # Predictions for the current training minibatch.
+    train_prediction = tf.nn.softmax(logits)
+
+    # Predictions for the test and validation, which we'll compute less often.
+    eval_prediction = tf.nn.softmax(model(eval_data))
 
   # Small utility function to evaluate a dataset by feeding batches of data to
   # {eval_data} and pulling the results from {eval_predictions}.
@@ -290,10 +301,9 @@ def main(_):
     return predictions
 
   # Create a local session to run the training.
-  hooks = [tf.train.StopAtStepHook(last_step=1000000)]
-
   start_time = time.time()
-  with tf.train.MonitoredTrainingSession(master=server.target, is_chief=True) as sess:
+
+  with tf.train.MonitoredTrainingSession(master=server.target, is_chief=(FLAGS.index == 0)) as sess:
     # Run all the initializers to prepare the trainable parameters.
     #sess.run(tf.global_variables_initializer())
     #print('Initialized!')
@@ -319,17 +329,20 @@ def main(_):
                                       feed_dict=feed_dict)
         elapsed_time = time.time() - start_time
         start_time = time.time()
-        print('Step %d (epoch %.2f), %.1f ms' %
-              (step, float(step) * BATCH_SIZE / train_size,
-               1000 * elapsed_time / EVAL_FREQUENCY))
-        print('Minibatch loss: %.3f, learning rate: %.6f' % (l, lr))
-        print('Minibatch error: %.1f%%' % error_rate(predictions, batch_labels))
-        print('Validation error: %.1f%%' % error_rate(eval_in_batches(validation_data, sess), validation_labels))
+
+        message = "[WORKER:%d]\n" %FLAGS.index
+        message += "Step %d (epoch %.2f), %.1f ms\n" % (step, float(step) * BATCH_SIZE / train_size, 1000 * elapsed_time / EVAL_FREQUENCY)
+        message += "Minibatch loss: %.3f, learning rate: %.6f\n" % (l, lr)
+        message += "Minibatch error: %.1f%%\n" % error_rate(predictions, batch_labels)
+        message += "Validation error: %.1f%%" % error_rate(eval_in_batches(validation_data, sess), validation_labels)
+        print(message)
         sys.stdout.flush()
 
     # Finally print the result!
-    test_error = error_rate(eval_in_batches(test_data, sess), test_labels)
-    print('Test error: %.1f%%' % test_error)
+    if FLAGS.index == 0:
+      test_error = error_rate(eval_in_batches(test_data, sess), test_labels)
+      print('Test error: %.1f%%' % test_error)
+
     if FLAGS.self_test:
       print('test_error', test_error)
       assert test_error == 0.0, 'expected 0.0 test_error, got %.2f' % (test_error,)
@@ -347,6 +360,10 @@ if __name__ == '__main__':
       default=False,
       action='store_true',
       help='True if running a self test.')
+  parser.add_argument("--index", 
+      default=0,
+      help="Index of the worker",
+      type=int)
 
   FLAGS, unparsed = parser.parse_known_args()
   tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
